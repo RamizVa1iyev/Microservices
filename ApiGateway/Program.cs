fde23d0148418beab7f1fc1d990de538265800c5
Ramiz Veliyev <vramiz001@gmail.com>
2023-03-31 02:25:05 +0400

    Test

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -23,7 +23,6 @@ builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
 
 // Add services to the container.
 
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
